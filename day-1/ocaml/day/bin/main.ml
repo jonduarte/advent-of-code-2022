669fb01e064f84c72891f7ae47aaab287a4988eb
Jonathan Duarte <jonathan.duarte@rocketmail.com>
2022-12-14 13:05:58 +0100

    Refactoring

--- day-1/ocaml/day/bin/main.ml
+++ day-1/ocaml/day/bin/main.ml
@@ -5,18 +5,16 @@ let split_groups = Str.split (Str.regexp "\n\n")
 let split_lines = Str.split (Str.regexp "\n")
 let list_of_ints = List.map ~f:int_of_string
 let sum = List.fold_left ~f:(+) ~init:0
-let invert f a b = f b a
-let take_inverted = invert List.take
+let (>>) f g x = g(f(x))
+let total_calories = split_lines >> list_of_ints >> sum
 
 let () =
   let result = content
     |> split_groups
-    |> List.map ~f:split_lines
-    |> List.map ~f:list_of_ints
-    |> List.map ~f:sum
+    |> List.map ~f:total_calories
     |> List.sort ~compare:Int.compare
     |> List.rev
-    |> take_inverted 3
+    |> Fn.flip (List.take) 3
     |> sum
 in
   print_endline (string_of_int result)
